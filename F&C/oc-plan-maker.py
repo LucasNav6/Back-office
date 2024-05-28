547505db68ef5447c5cb2828fad1e1bb5d5e2a2c
lucasnav6 <lucasnav6@gmail.com>
2024-05-28 21:55:28 +0000

    agregar mejoras al contructor de facturación

--- F&C/oc-plan-maker.py
+++ F&C/oc-plan-maker.py
@@ -8,11 +8,12 @@ import json
 # Esta respuesta se consigue al correr: oc-reader-agent.py
 # ! BORRAR
 respuesta_anterior = {
-    "inicio_contrato": "01-05-2024",
-    "fin_contrato": "30-04-2026",
-    "divisa": "SOLES",
-    "forma_pago": "90 días o afiliación a factoring",
-    "comentario_adicional": "La selección del candidato estará sujeta a la evaluación técnica que realizará el área usuaria al total de candidatos presentados por los proveedores homologados durante el presente proceso.",
+    "inicio_contrato": "02.05.2024",
+    "fin_contrato": "10.01.2026",
+    "divisa": "DOLARES AMERICANO",
+    "forma_pago": "4 Cuotas",
+    "total_pagar": 101290.10,
+    "comentario_adicional": "El Contrato tendrá una vigencia desde el 01 de mayo de 2024 al 30 de abril del 2026. No obstante, a lo señalado en el numeral anterior, ambas PARTES por común acuerdo podrán prorrogar o ampliar el plazo estipulado en la presente cláusula, con una anticipación no menor a quince (15) días calendario de la fecha de vencimiento del presente Contrato, señalando el nuevo plazo para la ampliación de arrendamiento correspondiente.",
     "campos_completos": True
 }
 
@@ -37,6 +38,7 @@ agent_classifier_role = """
     
     - Pagos: La fechas de los pagos deben ser calculados teniendo en cuenta a la 
         "forma de pago" proporcionada. Es decir, si dice mensual, es una por cada mes.
+        [Agregar siempre la divisa adelante, ejemplo si es "SOLES" usar S/, si es "Dólares" usar USD/]
 
     - Categoria: Las unicas categorias permitidas son: "Inicio", "Entregable", "Culminación".
         Elegir la que mejor se adapte en cada caso.
@@ -46,11 +48,13 @@ agent_classifier_role = """
         
 
   ##! Ejemplos de respuestas deseadas
+  ## Ejemplo 1
   Entrada: {
         "inicio_contrato": "02-08-2020",
         "fin_contrato": "02-12-2020",
         "divisa": "SOLES",
         "forma_pago": "Pago mensual",
+        "total_pagar": 76322.40,
         "comentario_adicional": "",
         "campos_completos": true
     }
@@ -61,24 +65,59 @@ agent_classifier_role = """
             "fecha": "02/08/2020",
             "categoria: "Inicio"
             "concepto": "Pago de inicio de contrato",
+            "total": "S/ 25440.80"
         },
         {
             "fecha": "02/09/2020",
             "categoria: "Entregable"
             "concepto": "Entregable 1",
+            "total": "S/ 25440.80"
         },
         {
             "fecha": "02/10/2020",
             "categoria: "Culminación"
             "concepto": "Entregable final",
+            "total": "S/ 25440.80"
         }
-    ]
+    ],
+    "suma_pagos": "S/ 76322.40"
   }
+
+   ## Ejemplo 2 
+   Entrada: {
+        "inicio_contrato": "01-09-2021",
+        "fin_contrato": "No especificado",
+        "divisa": "DOLARES AMERICANO",
+        "forma_pago": "Pago contado",
+        "total_pagar": 10127.12,
+        "comentario_adicional": "",
+        "campos_completos": true
+    }
+  Salida: {
+    "periodo": "Periodo: Del 02/08/2020 al (Sin especificar)"
+    "pagos": [
+        {
+            "fecha": "01/09/2021",
+            "categoria: "Inicio"
+            "concepto": "Pago de inicio de contrato",
+            "total": "USD/ 10127.12"
+        },
+    ],
+    "suma_pagos": "USD/ 10127.12"
+  }
+
+
+
+  ## ! A TENER EN CUENTA
+  - El metodo de pago "contado", se abonara en un solo pago, es decir en "pagos"
+    debera haber un solo elemento con categoria "Inicio" y concepto "Pago de inicio
+    de contrato"
 
 
   ## ! RESTRICCIONES
   - La primera fecha de pagos debe ser siempre el dia de inicio de contrato
   - La fecha de culminación y entrega final, debe ser siempre la fecha de fin de contrato, aunque no cumpla con la forma de pago
+  - La suma de todos los "totales" que aparecen en pagos debe ser SIEMPRE igual al total a pagar.
 """
 neo = InstantNeo(api_key,model, agent_classifier_role, max_tokens=1200)
 
